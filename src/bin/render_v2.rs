f027b31a4405466750bf8814089827b86fcec3ee
tttzof351 <7voprosov@gmail.com>
2026-07-02 01:11:51 +0400

    Add bin for play with rendering

---
+++ src/bin/render_v2.rs
@@ -0,0 +1,6 @@
+// cargo run --bin render_v2
+use theseus::diff_renders;
+
+fn main() {
+    diff_renders::run();
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod agent;
 pub mod commands;
 pub mod common;
+pub mod diff_renders;
 mod feature_flags;
 pub mod input;
 pub mod logging;
